fac445834ff012ae0dd3ee15326876889f3fd9ed
Fabian <cfacosta.16@gmail.com>
2024-01-31 19:15:22 -0300

    chzfabian &#129322;

---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,110 @@
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { EmployeeService } from '../services/employee.service';
+import { MatTableModule} from '@angular/material/table';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-emp-add-edit',
+  standalone: true,
+  providers: [provideNativeDateAdapter(), EmployeeService],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSnackBarModule,
+  ],
+  templateUrl: './emp-add-edit.component.html',
+  styleUrl: './emp-add-edit.component.scss',
+})
+export class EmpAddEditComponent implements OnInit {
+  empForm: FormGroup;
+
+  Educacion: string[] = [
+    'Matriculación',
+    'Diploma',
+    'Intermedio',
+    'Graduado',
+    'Postgrado',
+  ];
+
+  constructor(
+    private _fb: FormBuilder,
+    private _empService: EmployeeService,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      NombreDePila: '',
+      Apellido: '',
+      Email: '',
+      FechadeNacimiento: '',
+      Genero: '',
+      Educacion: '',
+      Empresa: '',
+      Experiencia: '',
+      Salario: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService
+        .updateEmployee(this.data.id, this.empForm.value)
+        .subscribe({
+          next: (val: any) => {
+            console.log({val})
+            alert('');
+            this._coreService.openSnackBar('Empleado eliminado!');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      } else {
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            console.log({val})
+            alert('');
+            this._coreService.openSnackBar('Empleado Agregado Exitosamente!');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+
+      }
+    }
+  }
+}
