a507c979796987959a3b687052ad619155282f66
Fabian <cfacosta.16@gmail.com>
2024-01-31 21:49:24 -0300

    Update deploy in repo

---
+++ dist/crud-app/server/main.server.mjs
@@ -0,0 +1,10 @@
+import './polyfills.server.mjs';
+import {
+  main_server_default
+} from "./chunk-IQSKBMM5.mjs";
+import "./chunk-IO7EUAJH.mjs";
+import "./chunk-24VIC3GD.mjs";
+export {
+  main_server_default as default
+};
+//# sourceMappingURL=main.server.mjs.map
